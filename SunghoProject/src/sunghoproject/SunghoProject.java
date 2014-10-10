be48715387f91cc883bf64faee6bd04eb7ce5a37
alren1004 <sung.an@gallaudet.edu>
2014-10-09 20:25:49 -0400

    Fourth Commit

--- SunghoProject/src/sunghoproject/SunghoProject.java
+++ SunghoProject/src/sunghoproject/SunghoProject.java
@@ -18,6 +18,11 @@ public class SunghoProject {
     public static void main(String[] args) {
         // TODO code application logic here
         System.Out.Println("Hello World");
+        
+        
+        
+        
+        "Hello"
     }
     
 }
